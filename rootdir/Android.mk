bfb42b950383c8eb1f8b67932e72e36f77a7b9df
ashuk1109 <ashutoshkarwa123@gmail.com>
2020-12-08 16:31:41 +0530

    Update fstab path

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,6 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
+inc
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6785.rc
 LOCAL_MODULE_TAGS  := optional
@@ -37,5 +38,5 @@ LOCAL_MODULE       := fstab.mt6785
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6785
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
